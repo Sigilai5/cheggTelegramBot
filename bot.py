944d7c86d6848fbd5e1d4c0880b0103182578e9d
Sigilai5 <briansigilai@gmail.com>
2021-09-07 14:57:12 +0300

    added Elkana

--- bot.py
+++ bot.py
@@ -67,7 +67,8 @@ def echo(update, context):
     paid_users = {
         "Sigi Brian": 1672275813,
         "Japhet Moturi": 1788310499,
-        "Sigilai Brian": 63710714
+        # "Sigilai Brian": 63710714
+        "Elkana Chegg": 761114919
     }
 
     paid_users_chat_id = paid_users.values()
